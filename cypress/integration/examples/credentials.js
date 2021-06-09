58c814f5074c6981f525d4c8e37f2651b2cdb943
Ajay Mallamar <ajay.mallamar@loanmarketgroup.com>
2021-06-09 10:20:21 +1000

    Trying environment variables

--- cypress/integration/examples/credentials.js
+++ cypress/integration/examples/credentials.js
@@ -1,8 +1,10 @@
 it('logs in using env variables', () => {
-    const username = Cypress.env('fff')
+    const username = Cypress.env('UserName')
     const password = Cypress.env('Password')
   
     Cypress.log(username)
+    expect(username).to.equal('ajayachar@gmail.com')
     Cypress.log(password)
+    expect(password).to.equal('Test123')
     
   })
