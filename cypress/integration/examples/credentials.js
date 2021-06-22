a04c07c4c7fd61ed509fde9691fad09c1ba136f9
Ajay Mallamar <ajay.mallamar@loanmarketgroup.com>
2021-06-22 12:22:36 +1000

    Making nightly job

--- cypress/integration/examples/credentials.js
+++ cypress/integration/examples/credentials.js
@@ -1,8 +1,24 @@
-it('logs in using env variables', () => {
-    const username = Cypress.env('UserName')
-    const password = Cypress.env('Password')
 
-    expect(username).to.equal('ajayachar@gmail.com')
-    expect(password).to.equal('Test123')
-    
+/// <reference types="cypress" />
+
+const username = Cypress.env('UserName')
+const password = Cypress.env('Password')
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/actions')
   })
+
+  cy.pause
+  // https://on.cypress.io/interacting-with-elements
+
+  it('.type() - type into a DOM element', () => {
+    // https://on.cypress.io/type
+    cy.get('.action-email')
+      .type(username).should('have.value', 'ajayachar@gmail.com')
+
+    cy.get('.action-email')
+    .type(password).should('have.value', 'Test123')
+  })
+
+})
